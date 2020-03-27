57cfdfd84a012a3fd1e908b2966d0d25f2ae0f12
Enomar <rosie.jane.r.enomar@accenture.com>
2020-03-27 13:23:17 +0800

    changed access token api

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoicm9zaWVqYW5lZW5vbWFyIiwiYSI6ImNrODlxOXJpczA3MHczbWxmYTZ1djZvengifQ.yoGtbXSbIpwgWdppyExDcQ&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
