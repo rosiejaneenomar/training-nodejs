3c01aecc9facc7db9e3a87d23a4261ca535b0f53
Enomar <rosie.jane.r.enomar@accenture.com>
2020-03-27 12:03:40 +0800

    set up heroku

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -1,5 +1,6 @@
 const request = require('request')
 
+
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
 
